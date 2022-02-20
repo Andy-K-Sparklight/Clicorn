33be7cfadf8cf20e98b3086cef9ca8b9f5ef67d4
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2022-02-20 23:15:28 +0800

    feat(win): frameless window

--- src/main.c
+++ src/main.c
@@ -3,7 +3,13 @@
 #define WEBVIEW_HEADER
 #include "webview.h"
 #include "bind.h"
-#include "limits.h"
+
+#ifdef WIN32
+#include <direct.h>
+#else
+#include <gtk/gtk.h>
+#include <sys/stat.h>
+#endif
 
 #ifdef WIN32
 int WINAPI WinMain(HINSTANCE hInt, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
@@ -13,6 +19,12 @@ int main()
 {
 #endif
     webview_t w = webview_create(0, NULL);
+    void *nw = webview_get_window(w);
+
+#ifndef WIN32
+    gtk_window_set_decorated(nw, FALSE);
+#endif
+
     webview_set_title(w, "Clicorn (Alicorn PE)");
     webview_set_size(w, 960, 540, WEBVIEW_HINT_NONE);
     bindAll(w);
@@ -20,4 +32,10 @@ int main()
     webview_run(w);
     webview_destroy(w);
     return 0;
+}
+
+extern unsigned char pack_tar_xz[]; // It's always this name
+
+int extractSelf()
+{
 }
